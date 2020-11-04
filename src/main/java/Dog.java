def0233f633c53763d4840c9ae9bc6a0de53534e
wdu0 <23wdu@athenian.org>
2020-11-04 12:55:53 +0000

    Added some code to the methods

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,38 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
+    public Dog(String a, int b, String c) { 
+        super(a, b);
+        this.breed = c;
+    }
     
+    public Dog() {
+        super("puppy", 1);
+        this.breed = "Husky";
+    }
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Wolf! ");
+    }
 
     // toString method
-
+    public String toString() { 
+        String str = "Name: "+this.getName()+" Age: "+this.getAge()+" Breed: "+this.getBreed();
+        return str;
+    }
 
     // Getter
-
+    public String getBreed() { 
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed) { 
+        if (breed.trim().length() > 0) {
+            this.breed = breed;
+        }
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,13 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog d1 = new Dog();
+        System.out.println(d1.toString());
+        d1.makeNoise();
+        System.out.println(d1.getAge());
+
+        Dog d2 = new Dog("Yoyo", 5, "Teddy");
+        System.out.println(d2.toString());
+        d2.makeNoise();
+        System.out.println(d2.getBreed());
     }
 }
